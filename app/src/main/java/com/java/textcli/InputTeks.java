900b07b2c10591125c61016521cb79b3bb84810f
ardhiesta <ardhi.mail@gmail.com>
2022-10-25 08:49:21 +0700

    add folder textcli

---
+++ app/src/main/java/com/java/textcli/InputTeks.java
@@ -0,0 +1,42 @@
+package com.java.textcli;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class InputTeks {
+	// method ini untuk input teks oleh user melalui keyboard
+	public String ketikTeks() {
+		System.out.println("Ketik kalimat yang akan disimpan :");
+		Scanner sc = new Scanner(System.in);
+		// ambil 1 kalimat, setelah tekan enter teks akan diambil
+		String str1 = sc.nextLine();
+		return str1;
+	}
+
+	// untuk menyimpan teks yang diketik user ke file
+	public void simpanTeks(String teks) {
+		// set up file and stream
+		// ganti path "/tmp/sample3.data" dengan alamat file di komputer anda
+		File outFile = new File("/tmp/sample3.data");
+		FileOutputStream outFileStream;
+		try {
+			outFileStream = new FileOutputStream(outFile);
+			PrintWriter outStream = new PrintWriter(outFileStream);
+			outStream.print(teks);
+			outStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		InputTeks it = new InputTeks();
+		// memanggil method ketikTeks
+		String kalimat = it.ketikTeks();
+		// memanggil method simpanTeks
+		it.simpanTeks(kalimat);
+	}
+}
