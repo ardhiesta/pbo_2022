84572897da4fb31cc26a782e22e3f1b214258470
ardhiesta <ardhi.mail@gmail.com>
2022-11-21 04:42:24 +0700

    menambahkan code dalam package p11

---
+++ app/src/main/java/com/java/p11/NamaThread.java
@@ -0,0 +1,8 @@
+package com.java.p11;
+
+public class NamaThread {
+	public static void main(String[] args) {
+		System.out.println("Nama thread utama : " 
+				+ Thread.currentThread().getName());
+	}
+}
